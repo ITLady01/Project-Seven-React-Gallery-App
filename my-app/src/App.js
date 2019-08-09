11cd76fdde5121ab7164e4b2cadf6d109526d7ff
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-08-09 12:19:59 -0400

    updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -25,8 +25,7 @@ export default class App extends Component {
 	// and store the responses inside the state of the component with 'setState'
 	componentDidMount() {
 		for (let i = 0; i < this.defaultImages.length; i++) {
-			axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this
-						.defaultImages[i]}&per_page=24&format=json&nojsoncallback=1`
+			axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this.defaultImages[i]}&per_page=24&format=json&nojsoncallback=1`
 				)
 				.then((response) => {
 					console.log(response);
