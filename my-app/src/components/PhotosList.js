3d3b674d0d68acd81817d2256df20c5341ea096a
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-08-09 11:29:13 -0400

    updates

--- my-app/src/components/PhotosList.js
+++ my-app/src/components/PhotosList.js
@@ -2,11 +2,25 @@ import React from 'react';
 
 // simple component that returns a list item with an image element inside of it
 const PhotosList = (props) => {
-  return (
-    <li>
-      <img src={props.url} alt="" />
-    </li>
-  );
-}
+	return (
+	  <li>
+	    <img src={props.url} alt="" />
+	  </li>
+	);
+
+	// let images = props.data.map((images) => (
+	// 	<PhotosList
+	// 		url={`https://farm${images.farm}.staticflickr.com/${images.server}/${images.id}_${images.secret}.jpg`}
+	// 		key={images.id}
+	// 	/>
+	// ));
+	// return (
+	// 	<div className="photo-container">
+	// 		{/* ternary operator that checks if the data object passed as props have items in it */}
+	// 		<h2>{props.data.length === 0 && props.match ? 'Sorry no match found!' : props.results}</h2>
+	// 		<ul>{images}</ul>
+	// 	</div>
+	// );
+};
 
 export default PhotosList;
